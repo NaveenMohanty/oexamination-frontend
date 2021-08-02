1628af7107a5c14a7c84af01c4e38c18127aea89
Naveen Mohanty <naveen.mohanty@namasys.co>
2021-08-02 21:42:31 +0530

    Resolution fixed

--- src/components/pages/hostexamjoin/index.jsx
+++ src/components/pages/hostexamjoin/index.jsx
@@ -113,6 +113,31 @@ const HostExamJoin = (props) => {
       config: {
         iceServers: [
           {
+            url: "turn:numb.viagenie.ca",
+            credential: "muazkh",
+            username: "webrtc@live.com",
+          },
+          {
+            url: "turn:192.158.29.39:3478?transport=udp",
+            credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+            username: "28224511:1379330808",
+          },
+          {
+            url: "turn:192.158.29.39:3478?transport=tcp",
+            credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+            username: "28224511:1379330808",
+          },
+          {
+            url: "turn:turn.bistri.com:80",
+            credential: "homeo",
+            username: "homeo",
+          },
+          {
+            url: "turn:turn.anyfirewall.com:443?transport=tcp",
+            credential: "webrtc",
+            username: "webrtc",
+          },
+          {
             urls: ["turn:13.250.13.83:3478?transport=udp"],
             username: "YzYNCouZM1mhqhmseWk6",
             credential: "YzYNCouZM1mhqhmseWk6",
